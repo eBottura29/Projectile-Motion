78af4b83eb7855c7f66e78ed0faefd838f0bb5f4
eBottura29 <botturaenrico@gmail.com>
2025-02-08 22:59:18 +0100

    working prototype

--- main.py
+++ main.py
@@ -2,6 +2,8 @@ from pg_extensions import *
 
 
 def calculate(v_i, theta, h_i, g, debug=False):
+    global v_ix, v_iy, t, d_x, d_y, h_max, t_max
+
     v_ix = v_i * math.cos(math.radians(theta))
     v_iy = v_i * math.sin(math.radians(theta))
 
@@ -34,12 +36,108 @@ def calculate(v_i, theta, h_i, g, debug=False):
     return v_ix, v_iy, t, d_x, d_y, h_max, t_max
 
 
+def render_ui():
+    global activated_before
+
+    values1 = Text(
+        f"Initial Velocity: {v_i:.2f} m/s",
+        Text.arial_16,
+        Vector2(2 * window.WIDTH // 5, 2 * window.HEIGHT // 5 - 0),
+        Text.top_right,
+        BLACK,
+    )
+    values1.render()
+
+    values2 = Text(
+        f"Angle: {theta:.2f}°",
+        Text.arial_16,
+        Vector2(2 * window.WIDTH // 5, 2 * window.HEIGHT // 5 - 32),
+        Text.top_right,
+        BLACK,
+    )
+    values2.render()
+
+    values3 = Text(
+        f"Initial Height: {h_i:.2f} m",
+        Text.arial_16,
+        Vector2(2 * window.WIDTH // 5, 2 * window.HEIGHT // 5 - 64),
+        Text.top_right,
+        BLACK,
+    )
+    values3.render()
+
+    values4 = Text(
+        f"Gravitational Force: {g:.2f} m/s^2",
+        Text.arial_16,
+        Vector2(2 * window.WIDTH // 5, 2 * window.HEIGHT // 5 - 96),
+        Text.top_right,
+        BLACK,
+    )
+    values4.render()
+
+    values5 = Text(
+        f"Time in Air: {t if activated_before else 0:.2f} s",
+        Text.arial_16,
+        Vector2(2 * window.WIDTH // 5, 2 * window.HEIGHT // 5 - 160),
+        Text.top_right,
+        BLACK,
+    )
+    values5.render()
+
+    values6 = Text(
+        f"Distance: {d_x if activated_before else 0:.2f} m",
+        Text.arial_16,
+        Vector2(2 * window.WIDTH // 5, 2 * window.HEIGHT // 5 - 192),
+        Text.top_right,
+        BLACK,
+    )
+    values6.render()
+
+    values7 = Text(
+        f"Maximum Height Above Initial Height: {h_max if activated_before else 0:.2f} m",
+        Text.arial_16,
+        Vector2(2 * window.WIDTH // 5, 2 * window.HEIGHT // 5 - 224),
+        Text.top_right,
+        BLACK,
+    )
+    values7.render()
+
+    values8 = Text(
+        f"Time to Reach Maximum Height: {t_max if activated_before else 0:.2f} s",
+        Text.arial_16,
+        Vector2(2 * window.WIDTH // 5, 2 * window.HEIGHT // 5 - 256),
+        Text.top_right,
+        BLACK,
+    )
+    values8.render()
+
+    disclaimer = Text(
+        f"*animation not to scale",
+        Text.arial_16,
+        Vector2(2 * window.WIDTH // 5, 2 * window.HEIGHT // 5 - 288),
+        Text.top_right,
+        BLACK,
+    )
+    disclaimer.render()
+
+    if not activated_before:
+        tutorial = Text(
+            f"Press the Space Bar to Simulate",
+            Text.arial_24,
+            Vector2(-2 * window.WIDTH // 5, 2 * window.HEIGHT // 5),
+            Text.top_left,
+            BLACK,
+        )
+        tutorial.render()
+
+
 def start():
-    global position, v_i, theta, h_i, g, r, v_ix, v_iy, t, d_x, d_y, h_max, t_max, velocity, clicked, scale, positions
+    global position, v_i, theta, h_i, g, r, velocity, clicked, scale, positions, activated_before
     window = get_window()
 
     velocity = Vector2()
     clicked = False
+    activated_before = False
 
     v_i = 28  # m/s
     theta = 30  # deg
@@ -51,17 +149,18 @@ def start():
     position = Vector2(-window.WIDTH // 2 + r, 0 if h_i > 0 else -window.HEIGHT // 2 + r)
     positions = []
 
-    v_ix, v_iy, t, d_x, d_y, h_max, t_max = calculate(v_i, theta, h_i, g)
-
 
 def update():
-    global window, position, velocity, clicked
+    global window, position, velocity, clicked, activated_before, v_ix, v_iy, t, d_x, d_y, h_max, t_max
     window = get_window()
     window.SURFACE.fill(WHITE.tup())
 
-    if input_manager.get_key_down(pygame.K_SPACE):
+    if input_manager.get_key_down(pygame.K_SPACE) and not activated_before:
         velocity = Vector2(10, 10)
         clicked = True
+        activated_before = True
+
+        v_ix, v_iy, t, d_x, d_y, h_max, t_max = calculate(v_i, theta, h_i, g)
 
     if clicked:
         velocity.y -= g * window.delta_time
@@ -83,17 +182,10 @@ def update():
     if h_i > 0:
         draw_rectangle(window.SURFACE, BLACK, Vector2(-window.WIDTH // 2, -r), Vector2(2 * r, window.HEIGHT), 2)
 
-    values = Text(
-        f"Initial Velocity: {v_i}\nAngle: {theta}\nInitial Height: {h_i}\nGravitational Force: {g}\n\nTime in Air: {t}\nDistance: {d_x}\nMaximum Height Above Initial Height: {h_max}\nTime to Reach Maximum Height: {t_max}",
-        Text.arial_32,
-        Vector2(4 * window.WIDTH // 5, 4 * window.HEIGHT // 5),
-        Text.top_right,
-        BLACK,
-    )
-    values.render()
+    render_ui()
 
     set_window(window)
 
 
 if __name__ == "__main__":
-    run(start, update, 800, 450, False, "Ball Launcher Simulation", 60)
+    run(start, update, 800, 450, False, "Projectile Motion Simulation", 60)
